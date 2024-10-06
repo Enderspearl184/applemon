53e2edc19893ce1756861657b63e310f6c7719c3
Enderspearl184 <73859349+Enderspearl184@users.noreply.github.com>
2024-10-05 20:21:58 -0700

    use the context and integration type enums lol

--- commands/play.js
+++ commands/play.js
@@ -1,22 +1,23 @@
 import util from "../util.js"
-import { SlashCommandBuilder } from "discord.js"
+import { SlashCommandBuilder, ApplicationIntegrationType, InteractionContextType } from "discord.js"
 
 const data = new SlashCommandBuilder()
 .setName('play')
 .setDescription('i wanna play pokemon crystal :D')
+.setIntegrationTypes([
+    ApplicationIntegrationType.GuildInstall,
+    ApplicationIntegrationType.UserInstall
+])
+.setContexts([
+    InteractionContextType.BotDM,
+    InteractionContextType.Guild,
+    InteractionContextType.PrivateChannel
+])
 
 async function execute(interaction) {
     console.log('executing play command')
     await interaction.deferReply()
-    const {embed, attachment} = util.createAttachmentsAndEmbed()
-    const components = util.createComponents()
-    await interaction.editReply({
-        embeds: [
-            embed
-        ],
-        files: [attachment],
-        components
-    })
+    await util.updateInteraction(interaction)
 }
 
 export {data, execute};
